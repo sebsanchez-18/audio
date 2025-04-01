68b9cd636f257856f5c5e4a31a4eb50a8b984d24
sebsanchez-18 <sebastianes1312@gmail.com>
2025-04-01 14:23:32 -0400

    Update audio-play.py

--- audio-play.py
+++ audio-play.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.mixer.init()
-pygame.mixer.music.load("our-audio.mp3")
+pygame.mixer.music.load("Frank Ocean - Miss You So.mp3")
 pygame.mixer.music.set_volume(1.0)
 pygame.mixer.music.play()
 
